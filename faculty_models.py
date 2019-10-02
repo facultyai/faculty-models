0bab7932b0b9a20b0876faf118715e5d923d6c61
Andrew Crozier <wacrozier@gmail.com>
2019-10-02 10:56:01 +0100

    Tweak names to match promised snippet

--- faculty_models.py
+++ faculty_models.py
@@ -21,49 +21,47 @@ import mlflow.pyfunc
 import mlflow.tracking.artifact_utils
 
 
-def download(project_id, model_id, path=None, version_number=None):
+def download(project_id, model_id, path=None, version=None):
     artifact_path = _determine_artifact_path(
-        project_id, model_id, path, version_number
+        project_id, model_id, path, version
     )
     return mlflow.tracking.artifact_utils._download_artifact_from_uri(
         artifact_path
     )
 
 
-def load_mlmodel(project_id, model_id, path=None, version_number=None):
+def load_mlmodel(project_id, model_id, path=None, version=None):
     artifact_path = _determine_artifact_path(
-        project_id, model_id, path, version_number
+        project_id, model_id, path, version
     )
     return mlflow.pyfunc.load_model(artifact_path)
 
 
-def _determine_artifact_path(
-    project_id, model_id, path=None, version_number=None
-):
-    model_version = _get_model_version(project_id, model_id, version_number)
+def _determine_artifact_path(project_id, model_id, path=None, version=None):
+    model_version = _get_model_version(project_id, model_id, version)
     if path is None:
         return model_version.artifact_path
     else:
         return _append_subpath_to_uri(model_version.artifact_path, path)
 
 
-def _get_model_version(project_id, model_id, version_number=None):
+def _get_model_version(project_id, model_id, version=None):
     client = faculty.client("model")
-    versions = client.list_versions(project_id, model_id)
-    if version_number is None:
-        matching = versions[-1:]
+    model_versions = client.list_versions(project_id, model_id)
+    if version is None:
+        matching = model_versions[-1:]
     else:
-        matching = [v for v in versions if v.version_number == version_number]
+        matching = [v for v in model_versions if v.version_number == version]
 
     try:
-        [version] = matching
-        return version
+        [model_version] = matching
+        return model_version
     except ValueError:
         if len(matching) == 0:
             tpl = "No version of model {} with version number {} found"
         else:
             tpl = "Multiple versions of model {} with version number {} found"
-        raise ValueError(tpl.format(model_id, version_number))
+        raise ValueError(tpl.format(model_id, version))
 
 
 def _append_subpath_to_uri(uri, subpath):
